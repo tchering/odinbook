fe46f7388fa10b594c796f5bdc6e5c4d88b2a65a
tchering <sonam.sherpa.lama@gmail.com>
2024-11-08 13:02:23 +0100

    generated views for devise
    added name in signup form
    
    set role to user by default

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
+  before_create :set_default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  private
+
+  def set_default_role
+    self.role = "user"
+  end
 end
