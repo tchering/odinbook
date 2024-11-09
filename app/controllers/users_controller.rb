c762e53b103647ea3c40cb097721302cd6c58085
tchering <sonam.sherpa.lama@gmail.com>
2024-11-09 14:29:20 +0100

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @posts = @user.posts.includes(:author)
+    @posts = @user.posts.includes(:author).recent
     unless @user
       redirect_to users_path, notice: "No user found"
     end
