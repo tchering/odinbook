fdb9633da171e8a0fb3c97d1ac17e1ac3dab9050
tchering <sonam.sherpa.lama@gmail.com>
2024-11-10 22:25:35 +0100

    improved UI for post partial

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :post
+  belongs_to :post, counter_cache: true
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
   scope :recent, -> { order(created_at: :desc) }
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
-  validates :body, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :body, presence: true, length: { minimum: 5, maximum: 300 }
 
   scope :recent, -> { order(created_at: :desc) }
 
