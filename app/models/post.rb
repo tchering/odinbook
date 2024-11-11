23f414102cbd8f1deab14a28f15368dd8636a6a4
tchering <sonam.sherpa.lama@gmail.com>
2024-11-11 10:47:05 +0100

    branch to upload image  in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,13 @@ class Post < ApplicationRecord
   scope :recent, -> { order(created_at: :desc) }
 
   has_many :comments, dependent: :destroy
+
+  has_one_attached :image
+  validates :image, content_type: { in: ["image/png", "image/jpg", "image/jpeg", "image/gif"], message: "must be a PNG, JPG, JPEG or GIF" }, size: { less_than: 2.megabytes, message: "should be less than 2MB" }
+
+  def display_image
+    if image.attached?
+      image.variant(resize_to_limit: [500, 500])
+    end
+  end
 end
