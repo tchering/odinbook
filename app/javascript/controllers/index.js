1ea7b275ba2cadc71505aca687b34b7f674342a4
tchering <sonam.sherpa.lama@gmail.com>
2024-11-12 21:33:59 +0100

    removed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,5 +2,5 @@
 import { application } from "controllers/application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 eagerLoadControllersFrom("controllers", application);
-import NavbarController from "./navbar_controller";
-application.register("navbar", NavbarController);
+// import NavbarController from "./navbar_controller";
+// application.register("navbar", NavbarController);
