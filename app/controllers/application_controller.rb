58e101fe7d84507ffa5cb8269e0a44f8e11a1d11
tchering <sonam.sherpa.lama@gmail.com>
2024-11-15 23:40:04 +0100

    fix signout error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  before_action :load_notifications
+  before_action :load_notifications, if: :user_signed_in?  # Only load if user is signed in
 
   protected
 
