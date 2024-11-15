e6ae46e58e6338f559822718dcec36c112d8af42
tchering <sonam.sherpa.lama@gmail.com>
2024-11-15 22:10:33 +0100

    password visibility

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,9 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+import PasswordVisibility from '@stimulus-components/password-visibility'
+
+application.register('password-visibility', PasswordVisibility)
 eagerLoadControllersFrom("controllers", application);
 // import NavbarController from "./navbar_controller";
 // application.register("navbar", NavbarController);
--- config/importmap.rb
+++ config/importmap.rb
@@ -11,3 +11,4 @@ pin_all_from 'app/javascript/controllers', under: 'controllers'
 # pin_all_from "app/javascript/custom", under: "custom"
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
+pin "@stimulus-components/password-visibility", to: "@stimulus-components--password-visibility.js" # @3.0.0
