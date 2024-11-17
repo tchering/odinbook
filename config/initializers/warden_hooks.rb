df7277a4d62a57366e8be27589a1b174bbbcd05b
tchering <sonam.sherpa.lama@gmail.com>
2024-11-17 19:18:47 +0100

    test

--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -1,4 +1,10 @@
+# config/initializers/warden_hooks.rb
 Warden::Manager.after_set_user do |user, auth, opts|
   scope = opts[:scope]
   auth.cookies.signed["#{scope}.id"] = user.id
+end
+
+Warden::Manager.before_logout do |user, auth, opts|
+  scope = opts[:scope]
+  auth.cookies.signed["#{scope}.id"] = nil
 end
